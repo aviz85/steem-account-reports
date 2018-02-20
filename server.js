d523660bb6d43fc6d887f73efe5b825f0313eb2b
aviz85 <avizmaeir@gmail.com>
2018-02-20 15:20:19 +0200

    basic express socket.io bundle

---
+++ server.js
@@ -0,0 +1,32 @@
+var config = require('config');
+var api = require('./lib/steem-api.js');
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/bower_components'));  
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/config', function(req, res, next){
+    console.log('request config');
+    res.sendFile(__dirname + '/config/default.json');
+});
+
+io.on('connection', function(client) {  
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+    client.on('query', function(data) {
+	console.log('query: '+data);
+	client.emit('results','test results...');
+    });
+
+});
+
+server.listen(config.get('socket.port'), function(){
+	console.log('server: listening...');
+});
